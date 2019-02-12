873879f1cb8803b1c7c43f697c77e2cee0433829
Bryce Derring <31260185+Renfif@users.noreply.github.com>
2019-02-12 10:05:54 -0800

    Comments

---
+++ Robot Code/src/main/java/frc/controllers/ButtonPanel.java
@@ -0,0 +1,7 @@
+package frc.controllers;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class ButtonPanel{
+    //its the same implementation as a joystick with just buttons
+}
--- Robot Code/src/main/java/frc/lift/LiftControl.java
+++ Robot Code/src/main/java/frc/lift/LiftControl.java
@@ -2,22 +2,22 @@ package frc.lift;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.controllers.ButtonPanel;
-
+import frc.interfaces.LoopModule;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.ControlType;
 
 public class LiftControl implements LoopModule{
     private final Lift lift;
-    private final ButtonPanel button;
+    private final ButtonPanel buttons;
     private boolean winchUpButton = false;
     private boolean winchDownButton = false;
     private boolean liftDriveFWD = false;
     private boolean liftDriveBWD = false;
 
-    public LiftControl(Lift lift, ButtonPanel button){
+    public LiftControl(Lift lift, ButtonPanel buttons){
         this.lift = lift;
-        this.button = button;
+        this.buttons = buttons;
     }
     
     @Override
