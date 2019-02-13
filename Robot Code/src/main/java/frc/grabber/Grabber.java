c61533b9a2010070bef6ff7a084d928fce8929f2
Bryce Derring <31260185+Renfif@users.noreply.github.com>
2019-02-12 22:10:36 -0800

    Hatch Grabber & Intake

--- Robot Code/src/main/java/frc/grabber/Grabber.java
+++ Robot Code/src/main/java/frc/grabber/Grabber.java
@@ -7,16 +7,30 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
-public class Grabber{
+public class Grabber {
 
     private VictorSPX intake;
     private Solenoid gripperPiston;
     private Solenoid pokePistons;
 
-    public Grabber(){
+    public Grabber() {
         intake = new VictorSPX(RobotMap.intakeMotor);
         gripperPiston = new Solenoid(RobotMap.gripperPiston);
         pokePistons = new Solenoid(RobotMap.pokePistons);
 
+        intake.setNeutralMode(NeutralMode.Coast);
+
+    }
+
+    public void setIntake(double n) {
+        intake.set(ControlMode.PercentOutput, n);
+    }
+
+    public void setGrabber(boolean b) {
+        gripperPiston.set(b);
+    }
+
+    public void setPokers(boolean b) {
+        pokePistons.set(b);
     }
 }
--- Robot Code/src/main/java/frc/grabber/GrabberControl.java
+++ Robot Code/src/main/java/frc/grabber/GrabberControl.java
@@ -4,23 +4,37 @@ import frc.grabber.Grabber;
 import frc.interfaces.LoopModule;
 import frc.controllers.JoystickController;
 
-public class GrabberControl implements LoopModule{
+public class GrabberControl implements LoopModule {
     private final Grabber grabber;
-	private final JoystickController Joy;
+    private final JoystickController Joy;
 
-	private boolean grabberOpen;
+    private boolean grabberOpen;
 
-	public GrabberControl(Grabber grabber, JoystickController Joy) {
-		this.grabber = grabber;
-		this.Joy = Joy;
-		grabberOpen = false;
-    }
-    
-    public void init(){
-        
+    public GrabberControl(Grabber grabber, JoystickController Joy) {
+        this.grabber = grabber;
+        this.Joy = Joy;
+        grabberOpen = false;
     }
 
-    public void update(long delta){
+    @Override
+    public void init() {
+        grabberOpen = false;
+    }
 
+    @Override
+    public void update(long delta) {
+        if (Joy.hatUp()) {
+            grabber.setIntake(-1);
+        } else if (Joy.hatDown()) {
+            grabber.setIntake(1);
+        } else {
+            grabber.setIntake(0);
+        }
+
+        if (Joy.getButtonDown(1)) {
+            grabberOpen = !grabberOpen;
+        }
+
+        grabber.setGrabber(grabberOpen);
     }
 }
