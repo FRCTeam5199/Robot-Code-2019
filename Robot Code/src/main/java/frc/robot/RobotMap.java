8f982cb2c8c686ed4179d561daa469bd097000d7
Bryce Derring <31260185+Renfif@users.noreply.github.com>
2019-02-14 18:32:04 -0800

    new CAN ID's

--- Robot Code/src/main/java/frc/robot/RobotMap.java
+++ Robot Code/src/main/java/frc/robot/RobotMap.java
@@ -10,21 +10,22 @@ public class RobotMap {
 	public static final double wheelEncoderIPP = 3 * 6 * Math.PI / 2048;
 
 	//air
-	public static final int gearboxPiston = 3;
-	public static final int pokePistons = 2;
-	public static final int gripperPiston = 1;
+	public static final int gearboxPistonB = 0;
+	public static final int gearboxPistonA = 1;
+	public static final int pokePistons = 3;
+	public static final int gripperPiston = 2;
 
 
 	//CAN ID's (dont forget to set the controllers to these numbers, PDP is zero, PCM will be 11?)
 	public static final int wristMotor = 5;
 	public static final int eleMotor = 3;
 	public static final int elbowMotor = 4;
-	public static final int driveLeaderRight = 2;
-	public static final int driveLeaderLeft = 1;
-	public static final int	driveSlaveRightA = 6;
-	public static final int	driveSlaveLeftA = 7;
-	public static final int driveSlaveRightB = 8;
-	public static final int	driveSlaveLeftB = 9;
+	public static final int driveLeaderRight = 1;
+	public static final int driveLeaderLeft = 2;
+	public static final int	driveSlaveRightA = 7;
+	public static final int	driveSlaveLeftA = 6;
+	public static final int driveSlaveRightB = 9;
+	public static final int	driveSlaveLeftB = 8;
 
 	public static final int CANGyro = 10;
 
@@ -42,12 +43,13 @@ public class RobotMap {
 	public static final int climberFWD = 3;
 	public static final int climberBWD = 4;
 
-	public static final int elevatorHatchHigh = 5;
-	public static final int elevatorHatchMid = 6;
-	public static final int elevatorHatchLow = 7;
-	public static final int elevatorHatchGround = 8;
-	public static final int elevatorCargoHigh = 9;
-	public static final int elevatorCargoMid = 10;
-	public static final int elevatorCargoLow = 11;
-	public static final int elevatorCargoGround = 12;
+	public static final int hatchHigh = 5;
+	public static final int hatchMid = 6;
+	public static final int hatchLow = 7;
+	public static final int hatchGround = 8;
+	public static final int cargoHigh = 9;
+	public static final int cargoMid = 10;
+	public static final int cargoLow = 11;
+	public static final int cargoGround = 12;
+	
 }
