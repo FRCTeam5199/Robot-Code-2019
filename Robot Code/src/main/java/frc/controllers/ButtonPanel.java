dfda65d09c595acf42f614b15f00c252970b7bfe
Bryce Derring <31260185+Renfif@users.noreply.github.com>
2019-02-14 18:30:56 -0800

    Button Panel

--- Robot Code/src/main/java/frc/controllers/ButtonPanel.java
+++ Robot Code/src/main/java/frc/controllers/ButtonPanel.java
@@ -1,7 +1,21 @@
 package frc.controllers;
-
 import edu.wpi.first.wpilibj.Joystick;
 
 public class ButtonPanel{
-    //its the same implementation as a joystick with just buttons
+	private final Joystick button;
+
+	public ButtonPanel(int n) {
+		button = new Joystick(n);
+	}
+	public boolean getButton(int n) {
+		return button.getRawButton(n);
+	}
+
+	public boolean getButtonDown(int n) {
+		return button.getRawButtonPressed(n);
+	}
+
+	public boolean getButtonUp(int n){
+		return button.getRawButtonReleased(n);
+	}
 }
