6284ebc6e71707a8f224af14b06106aa434adfb8
Garrett Knuf <garrettknuf@cox.net>
2019-02-17 19:58:00 -0800

    Update Grabber.java

--- Robot Code/src/main/java/frc/grabber/Grabber.java
+++ Robot Code/src/main/java/frc/grabber/Grabber.java
@@ -3,7 +3,6 @@ package frc.grabber;
 import edu.wpi.first.wpilibj.Solenoid;
 import frc.robot.RobotMap;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
@@ -14,16 +13,10 @@ public class Grabber {
     private Solenoid pokePistons;
 
     public Grabber() {
-        intake = new VictorSPX(RobotMap.intakeMotor);
         gripperPiston = new Solenoid(RobotMap.gripperPiston);
         pokePistons = new Solenoid(RobotMap.pokePistons);
 
         intake.setNeutralMode(NeutralMode.Coast);
-
-    }
-
-    public void setIntake(double n) {
-        intake.set(ControlMode.PercentOutput, n);
     }
 
     public void setGrabber(boolean b) {
@@ -33,4 +26,5 @@ public class Grabber {
     public void setPokers(boolean b) {
         pokePistons.set(b);
     }
+    
 }
