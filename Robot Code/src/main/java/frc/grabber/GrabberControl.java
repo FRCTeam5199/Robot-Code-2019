269096408ed78449037778ea65583d85a4e8f937
Bryce Derring <31260185+Renfif@users.noreply.github.com>
2019-02-27 20:45:33 -0800

    Update GrabberControl.java

--- Robot Code/src/main/java/frc/grabber/GrabberControl.java
+++ Robot Code/src/main/java/frc/grabber/GrabberControl.java
@@ -15,7 +15,7 @@ public class GrabberControl implements LoopModule {
     private final ButtonPanel panel;
 
     private boolean hasHatch;
-    //private boolean guideOn;
+    // private boolean guideOn;
     private double lastTime;
     private int lastButton;
 
@@ -34,7 +34,7 @@ public class GrabberControl implements LoopModule {
         lastButton = -1;
     }
 
-    private boolean groundPickupActive () {
+    private boolean groundPickupActive() {
         if (lastButton == 3 || lastButton == 6) {
             return true;
         }
@@ -43,32 +43,28 @@ public class GrabberControl implements LoopModule {
 
     @Override
     public void update(long delta) {
-        
+
         // Cargo: Joystick
         if (Joy.hatUp()) {
-			grabber.setIntake(1);
-		} else if (Joy.hatDown()) {
-			grabber.setIntake(-1);
-		} else {
-			grabber.setIntake(0);
+            grabber.setIntake(1);
+        } 
+        else if (Joy.hatDown()) {
+            grabber.setIntake(-1);
+        } 
+        else {
+            grabber.setIntake(0);
         }
 
-        /* if (Joy.getHat() != -1){
-            if (Joy.getHat() == 180){
-                grabber.setIntake(1);
-            }
-            else if (Joy.getHat() == 0){
-                grabber.setIntake(-1);
-            }
-        }
-        else if (Joy.getHat() == -1){
-            grabber.setIntake(0);
-        } */
+        /*
+         * if (Joy.getHat() != -1){ if (Joy.getHat() == 180){ grabber.setIntake(1); }
+         * else if (Joy.getHat() == 0){ grabber.setIntake(-1); } } else if (Joy.getHat()
+         * == -1){ grabber.setIntake(0); }
+         */
 
         // Hatch: Trigger
-        if (Joy.getButtonDown(1)){
+        if (Joy.getButtonDown(1)) {
 
-            if(!hasHatch) {
+            if (!hasHatch) {
                 grabber.setGrabber(true);
                 grabber.setPokers(false);
                 hasHatch = true;
@@ -88,15 +84,15 @@ public class GrabberControl implements LoopModule {
             lastTime = System.currentTimeMillis();
         }
 
-        /* if (Joy.getButtonDown(1)) {
-			hasHatch = !hasHatch;
-		}
-        grabber.setGrabber(hasHatch); */
-        
-        /* if(panel.getButtonDown(4)){
-            guideOn = !guideOn;
-        }
-        grabber.setHatchGuide(guideOn); */
-            
+        /*
+         * if (Joy.getButtonDown(1)) { hasHatch = !hasHatch; }
+         * grabber.setGrabber(hasHatch);
+         */
+
+        /*
+         * if(panel.getButtonDown(4)){ guideOn = !guideOn; }
+         * grabber.setHatchGuide(guideOn);
+         */
+
     }
 }
