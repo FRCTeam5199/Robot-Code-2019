9f7bda1d99f02926f079827ac93edde7e847b80e
Bryce Derring <31260185+Renfif@users.noreply.github.com>
2019-04-05 22:22:29 -0700

    Update ArmConstants.java

--- Robot Code/src/main/java/frc/armevator/ArmConstants.java
+++ Robot Code/src/main/java/frc/armevator/ArmConstants.java
@@ -3,21 +3,18 @@ package frc.armevator;
 public class ArmConstants{
 
     public static final double a = 0;
-    public static final double b = 5;
+    //combot
+    // public static final double b = 5;
+    //practice bot
+    public static final double b = 3.5;
     //elbow pos, wrist pos
-    // public static final double[] cargoIntake = {56.81612660933514 + a,-114.00028228759766 + b};
     public static final double[] cargoIntake = {53.81612660933514 + a,-117.00028228759766 + b};
     public static final double[] hatchIntake = {92.57064943897481 + a,-60.999507904052734 + b};
-    // public static final double[] hatchIntake = {75.84788855727838 ,-56.95195388793945};
-    // public static final double[] cargo1 = {99.07784598214286 + a,-129.52658081054688 + b};
     public static final double[] cargo1 = {97.07784598214286 + a,-127.72658081054688 + b};
     public static final double[] cargo2 = {61.644046082788584 + a,-91.52286529541016 + b};
     public static final double[] cargo3 = {160.3743373325893 + a,-169.53048706054688 + b};
-    // public static final double[] hatch1 = {75.84788855727838 ,-56.95195388793945};
-    // public static final double[] hatch1 = {92.57064943897481 ,-60.999507904052734};
     public static final double[] hatch1 = {81.57064943897481 + a,-58.999507904052734 + b};
     public static final double[] hatch2 = {59.37285940987724 + a,-37.99993133544922 + b};
-    // public static final double[] hatch2 = {64.37285940987724 + a,-34.99993133544922 + b};
     public static final double[] hatch3 = {151.4171952228157 + a,-114.14411926269531 + b};
     public static final double[] cargoShip = {147.3584868761958 + a,-188.14996337890625 + b};
     public static final double[] drive = {0,0};
