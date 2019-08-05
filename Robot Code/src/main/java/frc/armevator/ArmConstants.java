70784fa2b1137ee8642e25d7155d6d38aaf964f3
Conor O'Brien <30542291+Baygull@users.noreply.github.com>
2019-08-04 19:05:46 -0700

    Finalized H1 setpoint
    Finalized setpoint for hatch level 1

--- Robot Code/src/main/java/frc/armevator/ArmConstants.java
+++ Robot Code/src/main/java/frc/armevator/ArmConstants.java
@@ -16,7 +16,7 @@ public class ArmConstants{
     public static final double[] cargo2 = {61.644046082788584 + a,-91.52286529541016 + b};
     public static final double[] cargo3 = {157.3743373325893 + a,-169.53048706054688 + b};
     //public static final double[] hatch1 = {81.57064943897481 + a,-58.999507904052734 + b}; PRE-CONOR
-    public static final double[] hatch1 = {92.57064943897481 + a,-60.999507904052734 + b};
+    public static final double[] hatch1 = {81.57064943897481 + a,-58.999507904052734 + b};
     public static final double[] hatch2 = {59.37285940987724 + a,-37.99993133544922 + b};
     public static final double[] hatch3 = {151.4171952228157 + a,-114.14411926269531 + b};
     public static final double[] cargoShip = {147.3584868761958 + a,-188.14996337890625 + b};
